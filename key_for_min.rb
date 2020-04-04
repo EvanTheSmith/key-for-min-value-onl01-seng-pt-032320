43a8f0c5cfca0557eeca722218d860bf5d34479c
Evan Smith <professionalemail@zoho.com>
2020-04-04 08:10:29 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(name_hash)
   first_run=0
   the_key=""
 
-  name_hash.each do |key,value|
+  name_hash.each do |key, value|
     if first_run=0
       lowest = value
       the_key = key
